6e0ea03762d4efae42a2f97cd51b358605923f7f
Sanket Mehta <mehtasankets@gmail.com>
2020-03-14 02:48:13 +0530

    Dialog form beautification

--- frontend/containers/ExpenseForm.js
+++ frontend/containers/ExpenseForm.js
@@ -6,11 +6,19 @@ import { inject, observer } from 'mobx-react'
 import Flatpickr from 'react-flatpickr'
 import { RadioGroup, Radio } from 'react-radio-group'
 import Expense from '../store/Expense'
-import { Modal, Button } from 'react-bootstrap'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 @inject('ExpenseStore')
 @observer
 class ExpenseForm extends Component {
+
+    componentWillMount() {
+        this.date = this.props.ExpenseStore.expense.date;
+        this.description = this.props.ExpenseStore.expense.description;
+        this.category = this.props.ExpenseStore.expense.category;
+        this.paidBy = this.props.ExpenseStore.expense.paidBy;
+        this.amount = this.props.ExpenseStore.expense.amount;
+    }
 
     addNewExpense = (e) => {
         e.preventDefault()
@@ -40,38 +48,56 @@ class ExpenseForm extends Component {
         return <Modal show={ExpenseStore.showForm} onHide={handleClose} animation={false}
             size="lg">
             <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
+                <Modal.Title>{this.props.ExpenseStore.actionType}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <form>
-                    <Flatpickr placeholder='enter date'
-                        defaultValue={ExpenseStore.expense.date.toISOString()} onChange={value => this.date = value} />
-                    <input type='input' className='description' placeholder='enter description'
-                        defaultValue={ExpenseStore.expense.description} onChange={e => this.description = e.target.value} />
-                    <select id="categories" name="categories"
-                        defaultValue={ExpenseStore.expense.category} onChange={e => this.category = e.target.value}>
-                        <option value="Food">Food</option>
-                        <option value="Bills">Bills</option>
-                        <option value="Entertainment">Entertainment</option>
-                        <option value="Clothes">Clothes</option>
-                        <option value="Travel">Travel</option>
-                        <option value="Fuel">Fuel</option>
-                        <option value="Misc">Misc</option>
-                    </select>
-                    <RadioGroup name="paidby" selectedValue={ExpenseStore.expense.paidBy} onChange={value => this.paidBy = value}>
-                        <Radio value="Sanket" />Sanket
-                    <Radio value="Priyanka" />Priyanka
-                </RadioGroup>
-                    <input type='number' placeholder='enter amount' step='0.01'
-                        defaultValue={ExpenseStore.expense.amount} onChange={e => this.amount = e.target.value} />
-                </form>
+                <Form>
+                    <Form.Group>
+                        <Flatpickr placeholder='enter date'
+                            defaultValue={this.date.toISOString()} onChange={value => this.date = value} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="text" placeholder="Enter description"
+                            defaultValue={this.description} onChange={e => this.description = e.target.value} />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control as="select"
+                            defaultValue={this.category} onChange={e => this.category = e.target.value}>
+                            <option value="Food">Food</option>
+                            <option value="Bills">Bills</option>
+                            <option value="Entertainment">Entertainment</option>
+                            <option value="Clothes">Clothes</option>
+                            <option value="Travel">Travel</option>
+                            <option value="Fuel">Fuel</option>
+                            <option value="Misc">Misc</option>
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Check defaultChecked={'Sanket' === this.paidBy} onChange={e => this.paidBy = e.target.value}
+                            type='radio'
+                            name='paidBy'
+                            value='Sanket'
+                            label='Sanket'
+                        />
+                        <Form.Check defaultChecked={'Priyanka' === this.paidBy} onChange={e => this.paidBy = e.target.value}
+                            type='radio'
+                            name='paidBy'
+                            value='Priyanka'
+                            label='Priyanka'
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Control type="number" placeholder="Enter amount" step='0.01'
+                            defaultValue={this.amount} onChange={e => this.amount = e.target.value} />
+                    </Form.Group>
+                </Form>
             </Modal.Body>
 
             <Modal.Footer>
                 <Button variant="secondary" onClick={handleClose}>Close</Button>
                 <Button variant="primary" onClick={e => this.addNewExpense(e)}>Save changes</Button>
             </Modal.Footer>
-        </Modal>
+        </Modal >
     }
 }
 export default ExpenseForm
