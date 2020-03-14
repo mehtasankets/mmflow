ac6c3cc47e85132b563b7c8f7ce03b6ee89f7a4b
Sanket Mehta <mehtasankets@gmail.com>
2020-03-15 01:29:49 +0530

    navbar changes

--- frontend/containers/Header.js
+++ frontend/containers/Header.js
@@ -1,12 +1,15 @@
 import './Header.css'
 import React, { Component } from 'react'
+import { Navbar} from 'react-bootstrap'
 
 class Header extends Component {
     render() {
-        return <div className="title_bar">
-            <div className="title">
-                Monthly Money Flow Tracker
-            </div>
+        return <div className='header-bar'>
+            <Navbar bg="dark" variant="dark" fixed='top'>
+                <Navbar.Brand href="/mmflow/">
+                    Monthly money flow
+                </Navbar.Brand>
+            </Navbar>
         </div>
     }
 }
