2e2831f1a980d3d0f2b1a4c43bc0f94c622ae6d2
Sanket Mehta <mehtasankets@gmail.com>
2020-03-15 19:08:27 +0530

    Migrate to ag-grid

--- frontend/containers/DataGrid.js
+++ frontend/containers/DataGrid.js
@@ -1,95 +1,82 @@
 import './DataGrid.css'
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 
 import React, { Component } from 'react'
 import { inject, observer } from 'mobx-react'
-import ReactDataGrid from 'react-data-grid'
+import { AgGridReact } from 'ag-grid-react'
+import { Button } from 'react-bootstrap'
 import Expense from '../store/Expense'
-import { FaRegTrashAlt, FaPencilAlt } from "react-icons/fa";
-
-const columns = [
-    { key: "actions", name: "Actions", width: 100, resizable: true },
-    { key: "id", name: "ID", width: 70, resizable: true },
-    { key: "date", name: "Date", width: 220, resizable: true },
-    { key: "description", name: "Description", width: 220, resizable: true },
-    { key: "category", name: "Category", width: 180, resizable: true },
-    { key: "paidBy", name: "Paid By", width: 130, resizable: true },
-    { key: "amount", name: "Amount", width: 130, resizable: true }
-];
+import { FaRegTrashAlt, FaPencilAlt } from "react-icons/fa"
 
 @inject('ExpenseStore')
 @observer
 class DataGrid extends Component {
 
+    constructor(props) {
+        super(props)
+        this.defaultColDef = {
+            resizable: false, sortable: true, filter: true
+        }
+        this.columns = [
+            {
+                field: "actions", headerName: "Actions", sortable: false, filter: false, checkboxSelection: true, width: 100,
+                cellRendererFramework: function (params) {
+                    function updateExpense(params) {
+                        const row = params.data
+                        props.ExpenseStore.expense = new Expense(row.id, row.date, row.description, row.category, row.paidBy, row.amount)
+                        props.ExpenseStore.actionType = "Update"
+                        props.ExpenseStore.showForm = true
+                    }
+                    function deleteExpense(params) {
+                        props.ExpenseStore.deleteExpenses([params.data.id])
+                    }
+                    return <div>
+                        <FaPencilAlt className='cell-action-icon' onClick={e => updateExpense(params)} />
+                        <FaRegTrashAlt className='cell-action-icon' onClick={e => deleteExpense(params)} />
+                    </div>
+                }
+            },
+            { field: "id", headerName: "ID", width: 80 },
+            { 
+                field: "date", headerName: "Date", 
+                valueFormatter: function (params) {
+                    return params.value.toISOString()
+              }, width: 300 },
+            { field: "description", headerName: "Description", width: 300 },
+            { field: "category", headerName: "Category", width: 200 },
+            { field: "paidBy", headerName: "Paid By", width: 200 },
+            { field: "amount", headerName: "Amount", width: 200 }
+        ];
+    }
+
     componentDidMount() {
         this.props.ExpenseStore.getExpenses();
     }
 
-    createRow = (expense) => {
-        if (expense == undefined)
-            return {}
-        return {
-            "id": expense.id,
-            "date": expense.date.toISOString(),
-            "description": expense.description,
-            "category": expense.category,
-            "paidBy": expense.paidBy,
-            "amount": expense.amount
-        }
-    }
-
-    getCellActions = (column, row) => {
-        const cellActions = {
-            actions: [
-                {
-                    icon: <FaPencilAlt />,
-                    callback: () => {
-                        this.props.ExpenseStore.expense = new Expense(row.id, row.date, row.description, row.category, row.paidBy, row.amount)
-                        this.props.ExpenseStore.actionType = "Update"
-                        this.props.ExpenseStore.showForm = true
-                    }
-                },
-                {
-                    icon: <FaRegTrashAlt />,
-                    callback: () => {
-                        this.props.ExpenseStore.deleteExpenses([row.id])
-                    }
-                }
-            ]
-        };
-        return cellActions[column.key];
-    }
-
-    onRowsSelected = rows => {
-        const selected = rows.map(r => r.row.id);
-        this.props.ExpenseStore.selectedExpenseIds.push(...selected)
+    onSelectionChanged = () => {
+        const selectedNodes = this.gridApi.getSelectedNodes()
+        const selectedIds = selectedNodes.map(node => node.data.id)
+        this.props.ExpenseStore.selectedExpenseIds = selectedIds
     };
 
-    onRowsDeselected = rows => {
-        const toRemove = rows.map(r => r.row.id)
-        this.props.ExpenseStore.selectedExpenseIds = this.props.ExpenseStore.selectedExpenseIds
-            .filter(x => toRemove.indexOf(x) < 0);
-    };
+    onGridReady = (params) => {
+        params.api.sizeColumnsToFit()
+    }
 
     render() {
         const { ExpenseStore } = this.props
         return <div className='data-grid'>
-            <ReactDataGrid
-                columns={columns}
-                rowGetter={i => this.createRow(ExpenseStore.expenses[i])}
-                rowsCount={ExpenseStore.getCount}
-                getCellActions={this.getCellActions}
-                rowSelection={{
-                    showCheckbox: true,
-                    onRowsSelected: this.onRowsSelected,
-                    onRowsDeselected: this.onRowsDeselected,
-                    selectBy: {
-                      keys: {
-                          rowKey: "id",
-                          values: this.props.ExpenseStore.selectedExpenseIds
-                      }  
-                    }
-                }}
-            />
+            <div className="ag-theme-balham" style={{height: 440}}>
+                <AgGridReact
+                    defaultColDef={this.defaultColDef}
+                    columnDefs={this.columns}
+                    rowData={ExpenseStore.expenses}
+                    rowSelection='multiple'
+                    onSelectionChanged={this.onSelectionChanged}
+                    onGridReady={this.onGridReady}
+                />
+            </div>
         </div>
     }
 }
