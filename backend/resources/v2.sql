aa505f1d83e1ccbeb4a9012003618e9678b2fa3b
Sanket Mehta <mehtasankets@gmail.com>
2021-05-30 15:55:58 +0530

    Reorder options, SQL Query

---
+++ backend/resources/v2.sql
@@ -0,0 +1,3 @@
+UPDATE expenses SET category = 'Groceries' WHERE category = 'Food';
+UPDATE expenses SET category = 'Trips' WHERE category = 'Travel';
+UPDATE expenses SET category = 'Transport' WHERE category = 'Fuel';
