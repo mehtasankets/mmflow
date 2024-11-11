0b70d120d2332a8b20a3d962c849b5d777f4196f
Sanket Mehta <1165862+mehtasankets@users.noreply.github.com>
2024-11-11 15:51:12 +0530

    ktor-html-builder upgrade

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-html-builder:$ktor_version")
+    implementation("io.ktor:ktor-html-builder:1.6.8")
     implementation("org.jetbrains:kotlin-css-jvm:1.0.0-pre.31-kotlin-1.2.41")
     implementation("org.xerial:sqlite-jdbc:3.30.1")
     implementation("com.fasterxml.jackson.core:jackson-core:2.10.3")
